c2235d12e18d7eeade2eb3a43f474d8330454b43
Daniel Sukoneck <daniel@sukoneck.com>
2024-07-15 16:21:35 -0700

    clean up awsup

--- awsup.sh
+++ awsup.sh
@@ -7,27 +7,19 @@
 set -eo pipefail
 echo "🆙 Starting awsup..."
 
-# Set the target directory
 TARGET_DIR="${HOME}/.aws"
 REPO_URL="https://github.com/0x416e746f6e/.aws.git"
 REPO_BRANCH="main"
 
-# Check if the config or credentials files already exist
-CONFIG_FILE="${TARGET_DIR}/config"
-CREDENTIALS_FILE="${TARGET_DIR}/credentials"
-
-# Get the current date for the directory names
 DATE=$(date +%Y-%m-%d_%H-%M-%S)
 BACKUP_DIR="${TARGET_DIR}/backup-${DATE}"
 TEMP_DIR="$( mktemp -d -t awsup-XXX )"
 
-# Check if Git is installed
 if ! command -v git &> /dev/null; then
     echo "🚫 Git is not installed. Please install Git and try again."
     exit 1
 fi
 
-# Backup the existing .aws directory
 handle_file_backup() {
     echo "🔄 Backing up ${TARGET_DIR} to ${BACKUP_DIR}..."
     mkdir -p "${BACKUP_DIR}"
@@ -39,8 +31,6 @@ handle_file_backup() {
         fi
     done
 }
-
-handle_file_backup
 
 handle_installation() {
     echo "🔄 Downloading and installing..."
@@ -67,5 +57,7 @@ handle_installation() {
     echo "✅ Installation complete! To finish setup, run:"
     echo "✨    . ~/.aws/helper.sh --setup"
 }
+
+handle_file_backup
 
 handle_installation
