615e055f9777cf9503e6c26e4c085fba383a6a96
Anton Bronnikov <anton@northernforest.nl>
2024-07-15 17:20:42 +0200

    chore: placate `shellcheck`

--- get-1fa-credentials.sh
+++ get-1fa-credentials.sh
@@ -5,22 +5,22 @@ set -e -o pipefail
 # Log the attempt
 TS=$( date +'%Y-%m-%dT%H:%M:%S%z' )
 PID=$$
-ARG=$( ps -ww -o args= -p $PID )
-PARG=$( ps -ww -o args= -p $PPID )
-PPPID=$( ps -o ppid= -p $PPID )
-PPARG=$( ps -ww -o args= -p $PPPID )
-PPPPID=$( ps -o ppid= -p $PPPID )
-PPPARG=$( ps -ww -o args= -p $PPPPID )
+ARG=$( ps -ww -o args= -p "${PID}" )
+PARG=$( ps -ww -o args= -p "${PPID}" )
+PPPID=$( ps -o ppid= -p "${PPID}" )
+PPARG=$( ps -ww -o args= -p "${PPPID}" )
+PPPPID=$( ps -o ppid= -p "${PPPID}" )
+PPPARG=$( ps -ww -o args= -p "${PPPPID}" )
 printf '{ "ts": "%s", "grandgrandparent": { "pid": %s, "command": "%s" }, "grandparent": { "pid": %s, "command": "%s" }, "parent": { "pid": %s, "command": "%s" }, "self": { "pid": %s, "command": "%s" } }\n' \
     "${TS}" \
     "${PPPPID}" "${PPPARG}" \
     "${PPPID}" "${PPARG}" \
     "${PPID}" "${PARG}" \
     "${PID}" "${ARG}" \
-  >> ${HOME}/.aws/get-1fa-credentials.log
+  >> "${HOME}/.aws/get-1fa-credentials.log"
 
 pwd=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-. $pwd/lib.sh
+. "${pwd}/lib.sh"
 
 get_1fa_token
--- lib.sh
+++ lib.sh
@@ -1,11 +1,13 @@
+#!/bin/bash
+
 AWS_MFA_DEVICE="arn:aws:iam::${AWS_ACCOUNT_ID}:mfa/${AWS_IAM_USERNAME}"
 AWS_USER_ARN="arn:aws:iam::${AWS_ACCOUNT_ID}:user/${AWS_IAM_USERNAME}"
 
 function get_1fa_token() {
   security find-generic-password \
-    -l "$AWS_USER_ARN" \
-    -a "$AWS_USER_ARN" \
-    -s "$AWS_USER_ARN" \
+    -l "${AWS_USER_ARN}" \
+    -a "${AWS_USER_ARN}" \
+    -s "${AWS_USER_ARN}" \
     -w
 }
 
@@ -21,7 +23,7 @@ function save_2fa_token() {
   local AWS_SESSION_TOKEN=$1
 
   if [[ -n "${DEBUG}" ]]; then
-    echo "New AWS_SESSION_TOKEN: ${AWS_SESSION_TOKEN}" >> ${HOME}/.aws/debug.log
+    echo "New AWS_SESSION_TOKEN: ${AWS_SESSION_TOKEN}" >> "${HOME}/.aws/debug.log"
   fi
 
   if [[ -z "${DEBUG}" ]]; then
@@ -42,29 +44,29 @@ function save_2fa_token() {
         -l "${AWS_MFA_DEVICE}" \
         -a "${AWS_MFA_DEVICE}" \
         -s "${AWS_MFA_DEVICE}" \
-      >> ${HOME}/.aws/debug.log 2>&1 || true
+      >> "${HOME}/.aws/debug.log" 2>&1 || true
 
     security add-generic-password \
         -l "${AWS_MFA_DEVICE}" \
         -a "${AWS_MFA_DEVICE}" \
         -s "${AWS_MFA_DEVICE}" \
         -w "${AWS_SESSION_TOKEN}" \
-      >> ${HOME}/.aws/debug.log
+      >> "${HOME}/.aws/debug.log"
     fi
 }
 
 function request_2fa_token() {
   echo "Logging into AWS..." >&2
 
-  if ykman=$( which ykman ); then
-    if [ -z "$( ykman list )" ]; then
+  if ykman=$( command -v ykman ); then
+    if [[ -z "$( ykman list )" ]]; then
       echo "Please insert your yubikey and hit <ENTER>..." >&2
       read
     fi
-    PIN=$( $ykman oath accounts code --single ${AWS_MFA_DEVICE} )
+    PIN=$( "${ykman}" oath accounts code --single "${AWS_MFA_DEVICE}" )
   else
     # TODO: Implement 1password integration
-    printf "Enter one-time password for \`${AWS_MFA_DEVICE}\`: " >&2
+    printf "Enter one-time password for \`%s\`: " "${AWS_MFA_DEVICE}" >&2
     read PIN
   fi
 
@@ -73,9 +75,9 @@ function request_2fa_token() {
   if ! AWS_SESSION_TOKEN=$( aws \
         --profile 1fa \
       sts get-session-token \
-        --serial-number ${AWS_MFA_DEVICE} \
-        --duration-seconds 3600 \
-        --token-code $PIN \
+        --serial-number "${AWS_MFA_DEVICE}" \
+        --duration-seconds 28800 \
+        --token-code "${PIN}" \
     | jq -r -c ".Credentials + { \"Version\": 1 }"
   ); then
     echo "Failed to retrieve AWS session token" >&2
@@ -84,5 +86,5 @@ function request_2fa_token() {
 
   echo "Done." >&2
 
-  printf "${AWS_SESSION_TOKEN}"
+  printf "%s" "${AWS_SESSION_TOKEN}"
 }
